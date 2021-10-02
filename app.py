a5bc8f01e11d6ce3707eb383a2a61fb9498e21ea
Chris Vo <cvo@rivian.com>
2021-10-01 18:10:51 -0700

    running from wsgi.py

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def handle():
     )
 
 
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     app.run(debug=False, host="0.0.0.0", port=8000)
